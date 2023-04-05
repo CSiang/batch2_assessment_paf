9213b83f6c28a16faf03e2efd00f1851bc54c73a
CSiang <cscorpio87@gmail.com>
2023-04-05 13:43:54 +0800

    after lunch

--- src/main/java/paf2022/batch2_assessment/controllers/TasksController.java
+++ src/main/java/paf2022/batch2_assessment/controllers/TasksController.java
@@ -1,30 +1,38 @@
 package paf2022.batch2_assessment.controllers;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import paf2022.batch2_assessment.Models.Task;
+import paf2022.batch2_assessment.services.TodoService;
 
 // TODO: Task 4, Task 8
-@RestController
+@Controller
 @RequestMapping(path = "/")
 public class TasksController {
 
-    // username=cscorpio87&description-0=eat&priority-0=3&dueDate-0=2023-03-28&description-1=swim&priority-1=1&dueDate-1=2023-04-10&description-2=game&priority-2=2&dueDate-2=2023-02-07
-    @PostMapping(path = "task", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> saveTask(@RequestBody MultiValueMap<String, String> form) throws ParseException{
+    @Autowired
+    TodoService todoSvc;
 
+    @PostMapping(path = "task")
+    public ModelAndView saveTask(@RequestBody MultiValueMap<String, String> form, Model model) 
+    throws Exception{
 
         List<String> keyset =  new LinkedList<>(form.keySet());
         List<Task> tasks = new LinkedList<>();
@@ -48,7 +56,20 @@ public class TasksController {
             System.out.printf("\n\nAdded task: %s", t.toString());
         }
 
-        return null;
+        if(todoSvc.upsertTask(tasks)){
+            
+            ModelAndView mav = new ModelAndView("result.html");
+            mav.addObject("username",tasks.get(0).getUserName());
+            mav.addObject("taskCount", tasks.size());
+            mav.setStatus(HttpStatus.ACCEPTED);
+
+            return mav;
+
+        } else {
+            ModelAndView mav = new ModelAndView("error.html", HttpStatus.INTERNAL_SERVER_ERROR);
+
+            return mav;
+        }
+
     }
-    
 }
