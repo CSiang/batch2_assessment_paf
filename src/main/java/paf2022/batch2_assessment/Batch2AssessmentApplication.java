5c6a01341fca2c8350568a9afb5099c1b7d19286
CSiang <cscorpio87@gmail.com>
2023-04-05 09:06:27 +0800

    initial commit

---
+++ src/main/java/paf2022/batch2_assessment/Batch2AssessmentApplication.java
@@ -0,0 +1,13 @@
+package paf2022.batch2_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Batch2AssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Batch2AssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/paf2022/batch2_assessment/Batch2AssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package paf2022.batch2_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batch2AssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
